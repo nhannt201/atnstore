ddf401425535a5148cac3983df1702240136d574
nhannt201 <trungnhan21.12@gmail.com>
2021-05-04 20:08:25 +0700

    reconfig

--- inc/config.php
+++ inc/config.php
@@ -1,10 +1,10 @@
 <?php
 
 class DB {
-	private $hostname = "ec2-54-167-152-185.compute-1.amazonaws.com";
-    private $username = "ecqsxulgbhyxbt";
-    private $password = "5085b01e99638c8cccd7b748f8d78e18e4cedfd5c923e169b21ba5f101abceb1";
-    private $database = "dcqa0np5l3i67k";
+	private $hostname = "";
+    private $username = "";
+    private $password = "";
+    private $database = "";
 	private $charset = "utf8";
 	public function __construct(){
 		if(!isset($_SESSION)) {
@@ -14,7 +14,7 @@ class DB {
 		if(!isset($this->db)){
 			//$t=time();
 			//$conn = mysqli_connect($this->hostname, $this->username, $this->password,$this->database);
-			$conn = pg_connect("host=ec2-54-167-152-185.compute-1.amazonaws.com port=5432 dbname=dcqa0np5l3i67k user=ecqsxulgbhyxbt password=5085b01e99638c8cccd7b748f8d78e18e4cedfd5c923e169b21ba5f101abceb1");
+			$conn = pg_connect("host=ec2-54-146-73-98.compute-1.amazonaws.com port=5432 dbname=d9em16ehhf5pme user=wofdatlexxslwy password=1666c34bee3841c53bfd8512e9a871179c3d7fd9beb920690162b79060bc7055");
 			//new PDO("pgsql:host=ec2-52-1-115-6.compute-1.amazonaws.com;port=5432;dbname=dfc9kg4tdm8436;user=wimolppypfmrfd;password=45bdbcb28245bfb01ce99e2a525bdab01b81e7aa922696c14984d800fcf9071b");
 			pg_set_client_encoding($conn, "UTF8");
 
