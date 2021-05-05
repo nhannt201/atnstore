af6e43015c8d9bfeba7f992bf2b7044f4c329cc4
nhannt201 <trungnhan21.12@gmail.com>
2021-05-05 16:40:17 +0700

    m

--- inc/function/post.php
+++ inc/function/post.php
@@ -3,7 +3,7 @@ class Post extends DB {
 	
 	//Add san pham
 	function addProduct($category, $tensp, $giatien, $linkanh, $mota, $cauhinh, $sale){
-		$query_it = "INSERT INTO product (category, name, price, img, descc, config, sale) VALUES ($category, '$tensp', $giatien, '$linkanh', '$mota', '$cauhinh', $sale)";
+		$query_it = "INSERT INTO product (category, name, price, img, descc, config, sale) VALUES ('$category', '$tensp', '$giatien', '$linkanh', '$mota', '$cauhinh', $sale)";
 		pg_query($this->db,$query_it);
 		echo '<script> alert("New product added!");</script>';
 	}
--- test.php
+++ test.php
@@ -5,3 +5,7 @@ $timee = date("d-m-Y");
 //echo $get->getFollowDate("21-04-2021",$timee, 1);
 
 $post->addProduct(1, "con ga", 2000000, "https://i.imgur.com/gXuoLXn.png", "day la mo taaaaaaaaaaa", "day la cau hinhhhhhhhhhh", 0);
+
+//heroku pg:psql postgresql-angular-94756 --app atntoystoree
+//INSERT INTO product (category, name, price, img, descc, config, sale) VALUES (1, 'con ga', 2000000, 'https://i.imgur.com/gXuoLXn.png', 'day la mo taaaaaaaaaaa', 'day la cau hinhhhhhhhhhh', 0)
+
