35f5c83f8165406ad6d7b3fa4d3dd76db8fb8952
nhannt201 <trungnhan21.12@gmail.com>
2021-05-05 09:11:08 +0700

    d

--- inc/function/post.php
+++ inc/function/post.php
@@ -143,12 +143,16 @@ class Post extends DB {
 						//Thêm đơn hàng mới
 						//$newporder = "INSERT INTO order_sp (custid, notes, list_sp, time, status) VALUES ('$id_us', '$notes', '$list_sp','$timee', 0)";
 						echo '<script>alert("insert");</script>';
-						$newporder = pg_query($this->db, "INSERT INTO order_sp (custid, store_id, notes, time, status) VALUES ('$id_us', '$store_id', '$notes','$timee', 0);
-						SELECT Currval('order_sp_custid_seq') LIMIT 1");
+						$newporder = pg_query($this->db, "INSERT INTO order_sp (custid, store_id, notes, time, status) VALUES ('$id_us', '$store_id', '$notes','$timee', 0)");
 						//$qkk = $newporder);
 						//$insert_row = pg_fetch_row($newporder);
-						$order_id = pg_last_oid($newporder);
-						echo '<script>alert('.$order_id .$newporder.');</script>';
+						if(pg_num_rows($newporder)) {
+							$id=pg_fetch_array($newporder,0,PGSQL_ASSOC);
+							$order_id = $id['custid'];
+						} else {
+							$order_id =  pg_last_oid($newporder);
+						}
+						echo '<script>alert('.$order_id .');</script>';
 						//$order_id = $this->db->insert_id; //Lay duoc ma don hang roi
 						//Bo sung cai moi. Them add vao order details
 						foreach($_SESSION['cart'] as $productid => $soluong)  { 
