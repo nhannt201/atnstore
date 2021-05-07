c536c8aff291e3a9d418d8f5d00a8786b897c7f8
nhannt201 <trungnhan21.12@gmail.com>
2021-05-07 17:20:05 +0700

    m

--- test_sql.php
+++ test_sql.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "103.27.238.234";
+$servername = "103.27.238.234:3306";
 $username = "nguyentr_blog";
 $password = "Nhan123456789";
 
@@ -10,5 +10,5 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully"; //s*lCRfeyn8P! nguyentrungnhan@103.27.238.234:2210
 ?>
